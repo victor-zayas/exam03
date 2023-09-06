2990accb460880eaa874e85fe4bf37cfe85b0c81
Victor Zayas <55438812+victor-zayas@users.noreply.github.com>
2023-09-06 12:31:17 +0200

    refactor

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -2,7 +2,7 @@
 #include<unistd.h>
 #include<stdarg.h>
 
-void	ft_putstr(char *str, int *len)
+void ft_putstr(char *str, int *len)
 {
 	if (!str)
 		str = "(null)";
@@ -10,47 +10,47 @@ void	ft_putstr(char *str, int *len)
 		*len += write(1, str++, 1);
 }
 
-void	ft_putnbr(long nb, int base, int *len)
+void	ft_putnbr(long nbr, int base, int *len)
 {
 	char *hex = "0123456789abcdef";
 
-	if (nb < 0)
+	if (nbr < 0)
 	{
-		nb *= -1;
+		nbr *= -1;
 		*len += write(1, "-", 1);
 	}
-	if (nb >= base)
+	if (nbr >= base)
 	{
-		ft_putnbr((nb / base), base, len);
-		ft_putnbr((nb % base), base, len);
+		ft_putnbr((nbr / base), base, len);
+		ft_putnbr((nbr % base), base, len);
 	}
 	else
-		*len += write(1, &hex[nb], 1);
+		*len += write(1, &hex[nbr], 1);
 }
 
-int	ft_printf(const char *format, ...)
+int	ft_printf(const char *str, ...)
 {
+	va_list	format;
 	int	len = 0;
-	va_list	ptr;
 
-	va_start(ptr, format);
-	while (*format)
+	va_start(format, str);
+	while (*str)
 	{
-		if (*format == '%' && ((*(format + 1) == 's') || (*(format + 1) == 'd') || (*(format + 1) == 'x')))
+		if (*str == '%' && ((*(str + 1) == 's') || (*(str + 1) == 'd') || (*(str + 1) == 'x')))
 		{
-			format++;
-			if (*format == 's')
-				ft_putstr(va_arg(ptr, char *), &len);
-			else if (*format == 'd')
-				ft_putnbr(va_arg(ptr, int), 10, &len);
-			else if (*format == 'x')
-				ft_putnbr(va_arg(ptr, unsigned int), 16, &len);
+			str++;
+			if (*str == 's')
+				ft_putstr(va_arg(format, char *), &len);
+			else if (*str == 'd')
+				ft_putnbr(va_arg(format, int), 10, &len);
+			else if (*str == 'x')
+				ft_putnbr(va_arg(format, unsigned int), 16, &len);
 		}
 		else
-			len += write(1, format, 1);
-		format++;
+			len += write(1, str, 1);
+		str++;
 	}
-	va_end(ptr);
+	va_end(format);
 	return(len);
 }
 
